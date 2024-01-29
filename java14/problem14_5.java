786acee12950a37cdb76879f3623d26114492508
Ayato <ktq2390318@stu.o-hara.ac.jp>
2024-01-29 11:44:22 +0900

    Commit

--- java14/problem14_5.java
+++ java14/problem14_5.java
@@ -6,12 +6,15 @@ class problem14_5 {
         public static void main(String[] args) {
             Scanner stdIn = new Scanner(System.in, "SHIFT-JIS");
 
+            StringBuilder str = new StringBuilder();
+
             System.out.print("内容１：");
-            StringBuilder str1 = new StringBuilder(stdIn.nextLine());
+            String str1 = stdIn.nextLine();
             System.out.print("内容２：");
-            StringBuilder str2 = new StringBuilder(stdIn.nextLine());
+            String str2 = stdIn.nextLine();
             System.err.println();
-            System.out.println(str1.append(str2));
+            str.append(str1);
+            System.out.println(str.append(str2));
 
         }
 }
